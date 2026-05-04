c2f740d7faff7947b1d813c9a75f612921764ad2
Ammar-Negm <ammarahmed7750@gmail.com>
2026-05-05 02:38:22 +0300

    اخر مرة بجد ان شاء الله

--- SWE Project/implementation (codeing)/app/views/staff/qc-inspection.php
+++ SWE Project/implementation (codeing)/app/views/staff/qc-inspection.php
@@ -6,7 +6,7 @@
   <title>QC Inspection - WareLogix</title>
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
-  <link rel="stylesheet" href="/Php project/SWE-Project/SWE Project/implementation (codeing)/public/assets/css/style.css">
+  <link rel="stylesheet" href="<?= BASE_URL ?>assets/css/style.css">
 </head>
 <body>
 <div id="wrapper">
